2d8778a2f00ad632efd755d645be919135757107
Samuel Rivas <samuelrivas@gmail.com>
2010-09-07 15:46:33 +0200

    FUNCT first version of the cushion module.
    It implements a function to get all db names from a CouchDB server

---
+++ src/cushion.erl
@@ -0,0 +1,40 @@
+%%%-------------------------------------------------------------------
+%%% Copyright 2010 Samuel Rivas <samuelrivas@gmail.com>
+%%%
+%%% This file is part of Cushion.
+%%%
+%%% Cushion is free software: you can redistribute it and/or modify it under
+%%% the terms of the GNU General Public License as published by the Free
+%%% Software Foundation, either version 3 of the License, or (at your option)
+%%% any later version.
+%%%
+%%% Cushion is distributed in the hope that it will be useful, but WITHOUT ANY
+%%% WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+%%% FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+%%% details.
+%%%
+%%% You should have received a copy of the GNU General Public License along with
+%%% Cushion.  If not, see <http://www.gnu.org/licenses/>.
+%%%-------------------------------------------------------------------
+%%%-------------------------------------------------------------------
+%%% @author Samuel Rivas <samuelrivas@gmail.com>
+%%% @copyright (C) 2010, Samuel Rivas
+%%% @doc This is the main module of the cushion application, containing the
+%%% public API to talk to CouchDB instances
+%%%
+%%% @end
+%%% Created :  7 Sep 2010 by Samuel Rivas <samuelrivas@gmail.com>
+%%%-------------------------------------------------------------------
+-module(cushion).
+
+-export([new_access/2, get_dbs/1]).
+
+new_access(Host, Port) ->
+    {Host, Port}.
+
+get_dbs({Host, Port}) ->
+    [binary_to_list(Db)
+     || Db <- cushion_json:json2erl(
+                binary_to_list(cushion_couch_api:get_dbs(Host, Port)))].
+
+
