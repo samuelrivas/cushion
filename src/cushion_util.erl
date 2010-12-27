302c95808e38ce8b31b8423add2467c2854f1508
Samuel Rivas <samuelrivas@gmail.com>
2010-12-27 17:04:10 +0100

    FUNCT added functions to encode/decode unicode
    Like the unicode module but raising errors

--- src/cushion_util.erl
+++ src/cushion_util.erl
@@ -30,7 +30,7 @@
 -module(cushion_util).
 
 -export([start_app/1, format/2, untuple/1, get_value/2, get_value/3,
-         app_modules/1]).
+         app_modules/1, unicode_to_binary/1, binary_to_unicode/1]).
 
 %%--------------------------------------------------------------------
 %% @doc Starts an application and all its dependencies.
@@ -124,3 +124,40 @@ app_modules(App) ->
           file:consult(
             code:where_is_file(cushion_util:format("~p.app", [App])))),
     get_value(modules, Params).
+
+%%--------------------------------------------------------------------
+%% @doc Converts a list of unicode characters into a utf-8 binary.
+%%
+%% The main difference with the unicode module is that this function raises an
+%% error when `L' contains non-unicode characters (either values greater than
+%% 0x10FFFF, values in the interval 0xD800--0xD8FF, or 0xFFFE and 0xFFFF.
+%%
+%% @spec ([int()]) -> binary()
+%% @end
+%%--------------------------------------------------------------------
+unicode_to_binary(L) ->
+    case unicode:characters_to_binary(L, unicode, utf8) of
+        {error, _Good, Bad} ->
+            erlang:error({bad_unicode, Bad});
+        What ->
+            What
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc Converts a utf-8 binary into a list of unicode characters
+%%
+%% The main difference with the unicode module is that this function raises an
+%% error when `B' is not an utf-8 encoded binary
+%%
+%% @spec (binary()) -> [int()]
+%% @end
+%%--------------------------------------------------------------------
+binary_to_unicode(B) ->
+    case unicode:characters_to_list(B, utf8) of
+        {error, _Good, Bad} ->
+            erlang:error({bad_unicode, Bad});
+        {incomplete, _Good, Bad} ->
+            erlang:error({bad_unicode, Bad});
+        What ->
+            What
+    end.
